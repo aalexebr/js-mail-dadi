3acac0c0c12de65d67a74aeea66309e6c19c0ebd
aalexebr <alex.ebr@icloud.com>
2023-06-15 13:41:52 +0200

    general layout

---
+++ mail/js/script.js
@@ -0,0 +1,10 @@
+const mailList = ['a','b','c','d','e',]
+
+const form = document.getElementById('addEmailForm');
+
+form.addEventListener('submit',
+    function(event) {
+        event.preventDefault();
+
+    }
+);
