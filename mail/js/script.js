2d4ef669d1c47a8f86725dbfa704360d176acfe1
aalexebr <alex.ebr@icloud.com>
2023-06-15 13:51:02 +0200

    mail correct?

--- mail/js/script.js
+++ mail/js/script.js
@@ -5,6 +5,16 @@ const form = document.getElementById('addEmailForm');
 form.addEventListener('submit',
     function(event) {
         event.preventDefault();
-
+        let userEmail = document.getElementById('addEmail').value
+        
+        
+        for (let i = 0; i < mailList.length; i++) {
+            console.log('-', mailList[i]);
+            if (userEmail == mailList[i]){
+                alert('yes')
+            }
+            
+        }
+        
     }
 );
